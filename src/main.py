8a311f6acc45f3a203cbd3bcd336f519fd5e849d
Ahssan Javed <ahssanja@cse220-04.cse.buffalo.edu>
2022-07-19 09:06:06 -0400

    fixed some syntax errors

--- src/main.py
+++ src/main.py
@@ -7,13 +7,13 @@ password = input()
 word = ""
 index = 0
 
-dict = {"1","2","3","4","5","6","7","8","9","0"} + list(string.ascii_lowercase) + list(string.ascii+uppercase)
+dict = ["1","2","3","4","5","6","7","8","9","0"] + list(string.ascii_lowercase) + list(string.ascii_uppercase)
 
 while(word != password):
     for char in dict:
-        print(word+char)
+        print(word + char)
         time.sleep(0.03)
-        if(char == password[indez]):
+        if(char == password[index]):
             word = word + char
             index = index + 1
             break
