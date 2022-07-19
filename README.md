f66cea1017cb99af7365814a1072d68847bba555
Ahssan Javed <ahssanja@cse220-04.cse.buffalo.edu>
2022-07-19 08:57:30 -0400

    added code and more comments to the README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BruteForceCharByChar
+
+A cool visualizer of brute forcing a string character by character.
---
+++ src/main.py
@@ -0,0 +1,21 @@
+import string
+import time
+
+print("Enter your password")
+
+password = input()
+word = ""
+index = 0
+
+dict = {"1","2","3","4","5","6","7","8","9","0"} + list(string.ascii_lowercase) + list(string.ascii+uppercase)
+
+while(word != password):
+    for char in dict:
+        print(word+char)
+        time.sleep(0.03)
+        if(char == password[indez]):
+            word = word + char
+            index = index + 1
+            break
+
+print("\npassword is " + word)
